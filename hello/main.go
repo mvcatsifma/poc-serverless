8e3204f93f05ff9f2c0dfb064b76062ef931b0e4
Thomas Delnoij <mvcatsifma@gmail.com>
2019-04-28 17:16:29 +0200

    Simplify hello function

--- hello/main.go
+++ hello/main.go
@@ -9,10 +9,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
@@ -20,7 +16,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
+		"message": "Hello world!",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
